7883fe981e40187590e48daeddebae6aa8929b3b
Szalami72 <szalami72@gmail.com>
2024-06-09 17:43:11 +0200

    login comp started, fe works

--- src/app/shared/authentication/components/login/login.component.ts
+++ src/app/shared/authentication/components/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  email: string = '';
+  password: string = '';
+  stayedLogIn: boolean = false;
+
+  constructor(private authService: AuthService) { }
+
+  login() {
+    console.log(this.email, this.password, this.stayedLogIn);
+    //this.authService.login(this.email, this.password);
+  }
 }
