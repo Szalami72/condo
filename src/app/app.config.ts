9b222d4e86d773852933941bbb5d756b0ecbf933
Szalami72 <szalami72@gmail.com>
2024-06-10 21:27:26 +0200

    userdata stored in cookie

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient} from '@angular/common/http';
+import { LocalStorageService } from 'ngx-webstorage';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
      provideRouter(routes),
       provideHttpClient(),
+      LocalStorageService
     ]
 };
--- src/app/shared/authentication/components/login/login.component.ts
+++ src/app/shared/authentication/components/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { FormsModule } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [FormsModule],
+  providers: [CookieService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -13,23 +15,32 @@ export class LoginComponent {
 
   email: string = '';
   password: string = '';
-  stayedLogIn: boolean = false;
+  stayedLoggedIn: boolean = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private cookieService: CookieService) { }
 
-  login() {
-    this.authService.getUserData(this.email, this.password).subscribe(
-      response => {
+  async login() {
+    
+    try {
+      this.authService.getUserData(this.email, this.password).subscribe(response => {
         if (response.status === 'success') {
-          console.log('Login successful:', response.user);
-          // További teendők a sikeres bejelentkezés után, pl. token mentése, átirányítás stb.
+          console.log('Sikeres bejelentkezés:', response.user);
+  
+          // További teendők a sikeres bejelentkezés után
+          if (this.stayedLoggedIn) {
+            // Sütik létrehozása és beállítása
+            this.cookieService.set('currentUser', JSON.stringify(response.user));
+            console.log('Felhasználói adatok tárolva a sütiben.');
+          }
+  
+          // További műveletek a bejelentkezett felhasználóval
         } else {
-          console.error('Login failed:', response.message);
+          console.error('Sikertelen bejelentkezés:', response.message);
         }
-      },
-      error => {
-        console.error('Error:', error);
-      }
-    );
+      });
+    } catch (error) {
+      console.error('Hiba a bejelentkezés során:', error);
+    }
   }
+  
 }
